d141f6a50a08357d545ad58908ef1840309565f5
empchief <bjornmagnekristensen1999@gmail.com>
2024-01-04 16:33:03 +0100

    Refactor cart functionality and add-to-cart feature, also added so button on page works

--- cart/cart.py
+++ cart/cart.py
@@ -1,44 +1,49 @@
-from typing import Any
 from django.conf import settings
 from product.models import Product
-
 
 class Cart(object):
     def __init__(self, request):
         self.session = request.session
-        cart = self.session.get(settings.CART_SESSION_ID, None)
-        
+        cart = self.session.get(settings.CART_SESSION_ID)
+
         if cart is None:
-            self.session[settings.CART_SESSION_ID] = {}
-        
+            cart = self.session[settings.CART_SESSION_ID] = {}
+
         self.cart = cart
-        
+
     def __iter__(self):
-        for p in self.cart.keys():
-            self.cart(str(p))['product'] = Product.objects.get(id=p)
-        
+        product_ids = self.cart.keys()
+        products = Product.objects.filter(id__in=product_ids)
+
+        for product in products:
+            self.cart[str(product.id)]['product'] = product
+
+        for item in self.cart.values():
+            item['total_price'] = item['quantity'] * item['product'].price
+            yield item
+
     def __len__(self):
         return sum(item['quantity'] for item in self.cart.values())
-    
+
     def save(self):
         self.session[settings.CART_SESSION_ID] = self.cart
         self.session.modified = True
-    
+
     def add(self, product_id, quantity=1, update_quantity=False):
         product_id = str(product_id)
-        
         if product_id not in self.cart:
-            self.cart[product_id] = {
-                'product': product_id,
-                'quantity': quantity,
-            }
-        
+            self.cart[product_id] = {'quantity': 0}
+
         if update_quantity:
-            self.cart[product_id]['quantity'] += int(quantity)
-            if self.cart[product_id]['quantity'] == 0:
-                self.remove(product_id)
+            self.cart[product_id]['quantity'] = quantity
+        else:
+            self.cart[product_id]['quantity'] += quantity
+
+        if self.cart[product_id]['quantity'] <= 0:
+            self.remove(product_id)
+
         self.save()
-    
+
     def remove(self, product_id):
         if product_id in self.cart:
             del self.cart[product_id]
