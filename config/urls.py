7a43141d3404c1b4553115ad2d1722c85d37edd8
empchief <bjornmagnekristensen1999@gmail.com>
2024-01-07 18:41:01 +0100

    Add checkout functionality and international phone number input

--- config/urls.py
+++ config/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.auth import views
 from django.urls import path
-from cart.views import add_to_cart, cart
+from cart.views import add_to_cart, cart, checkout
 from mainpage.views import custom_login, custom_logout, frontpage, login_new, shop, signup
 from product.views import product
 
@@ -15,6 +15,7 @@ urlpatterns = [
     path('login/', custom_login, name='login'),
     path('shop/<slug:slug>/', product, name='product'),
     path('cart/', cart, name='cart'),
+    path('checkout/', checkout, name='checkout'),
     path('add_to_cart/<int:product_id>/', add_to_cart, name='add_to_cart'),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
