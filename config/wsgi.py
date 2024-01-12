7bfb27af75a7bff0a7c844772e3e75e2559d076d
empchief <bjornmagnekristensen1999@gmail.com>
2024-01-12 17:44:50 +0100

    Update DJANGO_SETTINGS_MODULE in wsgi.py

--- config/wsgi.py
+++ config/wsgi.py
@@ -1,8 +1,6 @@
-
 import os
-
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'saveplate.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+
 application = get_wsgi_application()
-app = application #add here
