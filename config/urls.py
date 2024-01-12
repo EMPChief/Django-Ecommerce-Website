745c1a397b9b169ff5a58894d51fb00a00e48720
empchief <bjornmagnekristensen1999@gmail.com>
2024-01-12 17:18:49 +0100

    Update URLs and WSGI configuration

--- config/urls.py
+++ config/urls.py
@@ -10,4 +10,8 @@ urlpatterns = [
     path('', include('product.urls')),
     path('', include('accounts.urls')),
     path('admin/', admin.site.urls),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+
+
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
