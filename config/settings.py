752119c26a3eb70eb42d641ec0b3558bad27313e
empchief <bjornmagnekristensen1999@gmail.com>
2024-01-12 17:55:38 +0100

    Fix static file configuration

--- config/settings.py
+++ config/settings.py
@@ -122,12 +122,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
-MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "ui/static")]
 
-STATIC_ROOT = os.path.join(BASE_DIR, "ui/staticfiles")
+STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+STATICFILES_DIRS = [BASE_DIR / 'ui/static']
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
