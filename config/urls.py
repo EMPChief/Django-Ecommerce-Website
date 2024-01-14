d8af727566b34d845367d824ee4ca68d11c1bd19
empchief <bjornmagnekristensen1999@gmail.com>
2024-01-14 18:04:52 +0100

    Add django-tinymce and remove CKEditor

--- config/urls.py
+++ config/urls.py
@@ -1,8 +1,7 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path, re_path, include
-# Import your apps' urls properly
+from django.urls import path, include
 
 urlpatterns = [
     path('', include('mainpage.urls')),
@@ -10,11 +9,9 @@ urlpatterns = [
     path('order/', include('order.urls')),
     path('', include('product.urls')),
     path('', include('accounts.urls')),
-    re_path(r'^grappelli/', include('grappelli.urls')),  # Assuming 'grappelli.urls' is correct
     path('admin/', admin.site.urls),
-    # For filebrowser, ensure you are including it correctly as per the documentation
-    # Example: path('filebrowser/', include('filebrowser.urls')), 
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
