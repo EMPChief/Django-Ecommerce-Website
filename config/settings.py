08bb90ae5ae819e98b8c85c44cd94c9dc82aeb0b
empchief <bjornmagnekristensen1999@gmail.com>
2024-01-14 15:13:46 +0100

    Update database SSL mode and debug mode

--- config/settings.py
+++ config/settings.py
@@ -18,7 +18,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', get_random_secret_key())
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG_MODE', False)
+DEBUG = os.environ.get('DEBUG_MODE', True)
 
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '127.0.0.1,localhost,*').split(',')
 
